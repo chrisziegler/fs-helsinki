8329d76dbcf08855b89385b3cebd883d2bb21243
Chris Ziegler <chrisrziegler@gmail.com>
2020-06-28 20:17:58 -0700

    added a couple files to this section

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,18 +1,27 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
+import './index.css'
+
+const Display = ({ count }) => <div>{count}</div>
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+)
 
 const App = () => {
   const [counter, setCounter] = useState(0)
 
   const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
   const setToZero = () => setCounter(0)
 
-  console.log(`rendering...`, counter)
   return (
-    <div>
-      <div>{counter}</div>
-      <button onClick={increaseByOne}>plus</button>
-      <button onClick={setToZero}>zero</button>
+    <div className="App">
+      <Display count={counter} />
+      <br />
+      <Button handleClick={increaseByOne} text="plus" />
+      <Button handleClick={setToZero} text="zero" />
+      <Button handleClick={decreaseByOne} text="minus" />
     </div>
   )
 }
