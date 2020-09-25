03c180fc9fd777de96e735a2d12376d5f5324343
Chris Ziegler <chrisrziegler@gmail.com>
2020-09-24 21:43:27 -0700

    part 3 MongoDB courseinfo

--- part2/phonebook/src/services/numbers.js
+++ part2/phonebook/src/services/numbers.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseURL = 'http://localhost:3001/persons'
+const baseURL = '/api/persons'
 
 // extracted request methods take the http request object return response data
 
