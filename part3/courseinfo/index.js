12027ed795d3930fa93f6f3a227c5429312e54af
Chris Ziegler <chrisrziegler@gmail.com>
2020-09-30 18:10:18 -0700

    added files to create and deploy heroku app

--- part3/courseinfo/index.js
+++ part3/courseinfo/index.js
@@ -17,7 +17,7 @@ app.get('/api/notes', (req, res) => {
 })
 
 // POST route -- add a note
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
   if (body.content === undefined) {
@@ -32,9 +32,12 @@ app.post('/api/notes', (request, response) => {
     date: new Date(),
   })
 
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
+  note
+    .save()
+    .then(savedNote => {
+      response.json(savedNote.toJSON())
+    })
+    .catch(error => next(error))
 })
 
 // GET route -- fetch individual note
@@ -80,6 +83,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
   next(error)
 }
