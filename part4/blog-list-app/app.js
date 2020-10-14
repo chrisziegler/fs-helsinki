968d13fed7e6c58875b07617b5d14f3cb7bda2d3
Chris Ziegler <chrisrziegler@gmail.com>
2020-10-13 23:17:33 -0700

    Refactor blogs list app into separate modules

---
+++ part4/blog-list-app/app.js
@@ -0,0 +1,25 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(console.log('Connected to MongoDB Atlas'))
+  .catch(error =>
+    console.log('Problem connecting to MongoDB Atlas', error.message),
+  )
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
