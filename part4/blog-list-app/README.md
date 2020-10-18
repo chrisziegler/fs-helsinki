94a1564ed577dc10340092679f3062ca3977f110
Chris Ziegler <chrisrziegler@gmail.com>
2020-10-17 21:41:58 -0700

    Add tests for blog list app

--- part4/blog-list-app/README.md
+++ part4/blog-list-app/README.md
@@ -1,5 +1,6 @@
 ## Blog List App - Part 4
 <ul>
 <li>exercises 4.1 -4.2</li>
-<li>exercises 4.3 -4.7</li>
+<li>exercises 4.3 -4.5*</li>
+<li>exercises 4.6* -4.7*</li>
 </ul>
--- part4/blog-list-app/utils/list_helper.js
+++ part4/blog-list-app/utils/list_helper.js
@@ -2,6 +2,28 @@ const dummy = array => {
   return 1
 }
 
-const totalLikes = array => array
+const totalLikes = array => {
+  return array.reduce((sum, item) => {
+    return (sum += item.likes)
+  }, 0)
+}
 
-module.exports = { dummy, totalLikes }
+const favoriteBlog = array => {
+  if (array.length === 0) {
+    return 0
+  }
+  const maxLikes = Math.max(...array.map(blog => blog.likes))
+  // A method for returning list of all items matching maxLikes
+  // return array
+  //   .filter(blog => blog.likes === maxLikes)
+  //   .map(({ _id, url, __v, ...keepAttrs }) => keepAttrs)
+
+  const mostLiked = array.find(blog => blog.likes === maxLikes)
+  return {
+    title: mostLiked.title,
+    author: mostLiked.author,
+    likes: mostLiked.likes,
+  }
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog }
