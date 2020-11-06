51c129c64f00ef78823fc386abb8cf5e9207e0c7
Chris Ziegler <chrisrziegler@gmail.com>
2020-11-05 23:57:19 -0800

    Add User models and controllers

--- part4/note-app/controllers/users.js
+++ part4/note-app/controllers/users.js
@@ -2,6 +2,13 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+// GET - fetch all users
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.json(users)
+})
+
+// POST - create a user
 usersRouter.post('/', async (request, response) => {
   const body = request.body
 
