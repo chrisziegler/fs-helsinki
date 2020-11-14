3fef3fd6ae67380d23b703a23f67c359764cd6ac
Chris Ziegler <chrisrziegler@gmail.com>
2020-11-13 23:22:48 -0800

    Extend middleware to handle JsonWebTokenError

--- part4/note-app/controllers/notes.js
+++ part4/note-app/controllers/notes.js
@@ -1,6 +1,15 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 // GET route -- fetch all notes
 notesRouter.get('/', async (req, res) => {
@@ -24,8 +33,12 @@ notesRouter.get('/:id', async (request, response) => {
 // POST route -- add a note
 notesRouter.post('/', async (request, response) => {
   const body = request.body
-
-  const user = await User.findById(body.userId)
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const note = new Note({
     content: body.content,
